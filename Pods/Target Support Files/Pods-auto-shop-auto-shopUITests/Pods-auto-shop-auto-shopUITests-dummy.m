93f2dbbd849eea214565a93b686ce8446b72c07e
IZZY <izzy@IZZYs-MacBook-Pro.local>
2024-01-28 09:17:02 -0800

    first commit

---
+++ Pods/Target Support Files/Pods-auto-shop-auto-shopUITests/Pods-auto-shop-auto-shopUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_auto_shop_auto_shopUITests : NSObject
+@end
+@implementation PodsDummy_Pods_auto_shop_auto_shopUITests
+@end
---
+++ auto-shop/auto_shopApp.swift
@@ -0,0 +1,17 @@
+//
+//  auto_shopApp.swift
+//  auto-shop
+//
+//  Created by IZZY on 28/1/2024.
+//
+
+import SwiftUI
+
+@main
+struct auto_shopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
